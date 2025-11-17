04f4853bb460c74328735c1b290fd04bf26b6b4f
Siller-Olena <sillerolena@gmail.com>
2025-11-17 10:57:40 +0100

    first commit

---
+++ src/components/Board.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Board = ({ squares, onClick }) => {
+  return (
+    <div className="board">
+      {squares.map((square, index) => (
+        <button
+          key={index}
+          className="square"
+          onClick={() => onClick(index)}
+        >
+          {square}
+        </button>
+      ))}
+    </div>
+  );
+};
+
+export default Board;
